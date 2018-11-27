71b4218d7db5be9957c90949bb7a9f72fb84db6a
Christoph Görn <goern@redhat.com>
2018-11-27 10:44:56 +0100

    added prometheus metric export for all things REST
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- thoth/management_api/api_v1.py
+++ thoth/management_api/api_v1.py
@@ -36,46 +36,52 @@ _LOGGER = logging.getLogger(__name__)
 _OPENSHIFT = OpenShift()
 
 
-def post_register_python_package_index(url: str, warehouse_api_url: str = None, verify_ssl: bool = True):
+def post_register_python_package_index(
+    url: str, warehouse_api_url: str = None, verify_ssl: bool = True
+):
     """Register the given Python package index in the graph database."""
     graph = GraphDatabase()
     graph.connect()
     graph.register_python_package_index(
         url=url,
         warehouse_api_url=warehouse_api_url,
-        verify_ssl=verify_ssl if verify_ssl is not None else True
+        verify_ssl=verify_ssl if verify_ssl is not None else True,
     )
     return {}, 201
 
 
-def post_solve_python(python_package: dict, version_specifier: str = None, debug: bool = False):
+def post_solve_python(
+    python_package: dict, version_specifier: str = None, debug: bool = False
+):
     """Register the given Python package in Thoth."""
     parameters = locals()
 
-    package_name = python_package.pop('package_name')
-    version_specifier = python_package.pop('version_specifier')
-    packages = package_name + (version_specifier if version_specifier else '')
+    package_name = python_package.pop("package_name")
+    version_specifier = python_package.pop("version_specifier")
+    packages = package_name + (version_specifier if version_specifier else "")
 
     graph = GraphDatabase()
     graph.connect()
     run_parameters = {
-        'packages': packages,
-        'indexes': graph.get_python_package_index_urls()
+        "packages": packages,
+        "indexes": graph.get_python_package_index_urls(),
     }
-    response, status_code = _do_run(run_parameters, _OPENSHIFT.run_solver, output=Configuration.THOTH_SOLVER_OUTPUT)
+    response, status_code = _do_run(
+        run_parameters, _OPENSHIFT.run_solver, output=Configuration.THOTH_SOLVER_OUTPUT
+    )
 
     # Handle a special case where no solvers for the given name were found.
-    if status_code == 202 and not response['analysis_id']:
+    if status_code == 202 and not response["analysis_id"]:
         if solver:
-            return {
-                'error': "No solver was run",
-                'parameters': parameters
-            }, 400
+            return {"error": "No solver was run", "parameters": parameters}, 400
         else:
-            return {
-                'error': "Please contact administrator - no solvers were installed",
-                'parameters': parameters
-            }, 500
+            return (
+                {
+                    "error": "Please contact administrator - no solvers were installed",
+                    "parameters": parameters,
+                },
+                500,
+            )
 
     return response, status_code
 
@@ -83,19 +89,21 @@ def post_solve_python(python_package: dict, version_specifier: str = None, debug
 def get_solve_python(analysis_id: str):
     """Retrieve the given solver result."""
     return _get_document(
-        SolverResultsStore, analysis_id,
-        name_prefix='solver-', namespace=Configuration.THOTH_MIDDLETIER_NAMESPACE
+        SolverResultsStore,
+        analysis_id,
+        name_prefix="solver-",
+        namespace=Configuration.THOTH_MIDDLETIER_NAMESPACE,
     )
 
 
 def get_solve_python_log(analysis_id: str):
     """Get solver log."""
-    return _get_job_log(locals(), 'solver', Configuration.THOTH_MIDDLETIER_NAMESPACE)
+    return _get_job_log(locals(), "solver", Configuration.THOTH_MIDDLETIER_NAMESPACE)
 
 
 def get_solve_python_status(analysis_id: str):
     """Get status of an ecosystem solver."""
-    return _get_job_status(locals(), 'solver', Configuration.THOTH_MIDDLETIER_NAMESPACE)
+    return _get_job_status(locals(), "solver", Configuration.THOTH_MIDDLETIER_NAMESPACE)
 
 
 def list_solve_python_results(page: int = 0):
@@ -106,58 +114,64 @@ def list_solve_python_results(page: int = 0):
 def list_solvers():
     """List available registered solvers."""
     # We are fine with 500 here in case of some OpenShift/configuration failures.
-    return {
-        'solvers': {'python': _OPENSHIFT.get_solver_names()},
-        'parameters': {}
-    }
+    return {"solvers": {"python": _OPENSHIFT.get_solver_names()}, "parameters": {}}
 
 
-def post_dependency_monkey_python(input: dict, seed: int = None, dry_run: bool = False, limit: int = None,
-                                  decision: str = None, debug: bool = False):
+def post_dependency_monkey_python(
+    input: dict,
+    seed: int = None,
+    dry_run: bool = False,
+    limit: int = None,
+    decision: str = None,
+    debug: bool = False,
+):
     """Run dependency monkey on the given application stack to produce all the possible software stacks."""
-    requirements = input.pop('requirements')
-    context = input.pop('context')
+    requirements = input.pop("requirements")
+    context = input.pop("context")
     parameters = locals()
-    parameters.pop('input')
+    parameters.pop("input")
 
     return _do_run(
         parameters,
         _OPENSHIFT.run_dependency_monkey,
         report_output=Configuration.THOTH_DEPENDENCY_MONKEY_REPORT_OUTPUT,
-        stack_output=Configuration.THOTH_DEPENDENCY_MONKEY_STACK_OUTPUT
+        stack_output=Configuration.THOTH_DEPENDENCY_MONKEY_STACK_OUTPUT,
     )
 
 
 def get_dependency_monkey_python_log(analysis_id: str):
     """Get dependency monkey container log."""
-    return _get_job_log(locals(), 'dependency-monkey-', Configuration.THOTH_MIDDLETIER_NAMESPACE)
+    return _get_job_log(
+        locals(), "dependency-monkey-", Configuration.THOTH_MIDDLETIER_NAMESPACE
+    )
 
 
 def get_dependency_monkey_python_status(analysis_id: str):
     """Get dependency monkey container status."""
-    return _get_job_status(locals(), 'dependency-monkey-', Configuration.THOTH_MIDDLETIER_NAMESPACE)
+    return _get_job_status(
+        locals(), "dependency-monkey-", Configuration.THOTH_MIDDLETIER_NAMESPACE
+    )
 
 
 def sync(secret: str, force_sync: bool = False):
     """Sync results to graph database."""
     parameters = locals()
     if secret != Configuration.THOTH_MANAGEMENT_API_TOKEN:
-        return {
-            'error': 'Wrong secret provided'
-        }, 401
+        return {"error": "Wrong secret provided"}, 401
 
-    return {
-        'sync_id': _OPENSHIFT.run_sync(force_sync=force_sync),
-        'parameters': parameters
-    }, 202
+    return (
+        {
+            "sync_id": _OPENSHIFT.run_sync(force_sync=force_sync),
+            "parameters": parameters,
+        },
+        202,
+    )
 
 
 def erase_graph(secret: str):
     """Clean content of the graph database."""
     if secret != Configuration.THOTH_MANAGEMENT_API_TOKEN:
-        return {
-            'error': 'Wrong secret provided'
-        }, 401
+        return {"error": "Wrong secret provided"}, 401
 
     adapter = GraphDatabase()
     adapter.connect()
@@ -168,7 +182,7 @@ def erase_graph(secret: str):
 
 def get_dependency_monkey_report(analysis_id: str) -> dict:
     """Retrieve a dependency monkey run report."""
-    parameters = {'analysis_id': analysis_id}
+    parameters = {"analysis_id": analysis_id}
 
     adapter = DependencyMonkeyReportsStore()
     adapter.connect()
@@ -176,15 +190,15 @@ def get_dependency_monkey_report(analysis_id: str) -> dict:
     try:
         document = adapter.retrieve_document(analysis_id)
     except NotFoundError:
-        return {
-            'parameters': parameters,
-            'error': f"Report with the given id {analysis_id} was not found"
-        }, 404
+        return (
+            {
+                "parameters": parameters,
+                "error": f"Report with the given id {analysis_id} was not found",
+            },
+            404,
+        )
 
-    return {
-        'parameters': parameters,
-        'report': document
-    }
+    return {"parameters": parameters, "report": document}
 
 
 def _do_listing(adapter_class, page: int) -> tuple:
@@ -194,26 +208,24 @@ def _do_listing(adapter_class, page: int) -> tuple:
     result = adapter.get_document_listing()
     # We will need to abandon this logic later anyway once we will be
     # able to query results on data hub side.
-    results = list(islice(result, page * PAGINATION_SIZE, page * PAGINATION_SIZE + PAGINATION_SIZE))
-    return {
-        'results': results,
-        'parameters': {'page': page}
-    }, 200, {
-        'page': page,
-        'page_size': PAGINATION_SIZE,
-        'results_count': len(results)
-    }
+    results = list(
+        islice(result, page * PAGINATION_SIZE, page * PAGINATION_SIZE + PAGINATION_SIZE)
+    )
+    return (
+        {"results": results, "parameters": {"page": page}},
+        200,
+        {"page": page, "page_size": PAGINATION_SIZE, "results_count": len(results)},
+    )
 
 
-def _get_document(adapter_class, analysis_id: str, name_prefix: str = None, namespace: str = None) -> tuple:
+def _get_document(
+    adapter_class, analysis_id: str, name_prefix: str = None, namespace: str = None
+) -> tuple:
     """Perform actual document retrieval."""
     # Parameters to be reported back to a user of API.
-    parameters = {'analysis_id': analysis_id}
+    parameters = {"analysis_id": analysis_id}
     if not analysis_id.startswith(name_prefix):
-        return {
-            'error': 'Wrong analysis id provided',
-            'parameters': parameters
-        }, 400
+        return {"error": "Wrong analysis id provided", "parameters": parameters}, 400
 
     try:
         adapter = adapter_class()
@@ -223,78 +235,91 @@ def _get_document(adapter_class, analysis_id: str, name_prefix: str = None, name
     except NotFoundError:
         if namespace:
             try:
-                status = _OPENSHIFT.get_job_status_report(analysis_id, namespace=namespace)
-                if status['state'] == 'running' or \
-                        (status['state'] == 'terminated' and status['exit_code'] == 0):
+                status = _OPENSHIFT.get_job_status_report(
+                    analysis_id, namespace=namespace
+                )
+                if status["state"] == "running" or (
+                    status["state"] == "terminated" and status["exit_code"] == 0
+                ):
                     # In case we hit terminated and exit code equal to 0, the analysis has just finished and
                     # before this call (document retrieval was unsuccessful, pod finished and we asked later
                     # for status). To fix this time-dependent issue, let's user ask again. Do not do pod status
                     # check before document retrieval - this solution is more optimal as we do not ask master
                     # status each time.
-                    return {
-                        'error': 'Analysis is still in progress',
-                        'status': status,
-                        'parameters': parameters
-                    }, 202
-                elif status['state'] == 'terminated':
-                    return {
-                        'error': 'Analysis was not successful',
-                        'status': status,
-                        'parameters': parameters
-                    }, 404
-                elif status['state'] in ('scheduling', 'waiting'):
-                    return {
-                        'error': 'Analysis is being scheduled',
-                        'status': status,
-                        'parameters': parameters
-                    }, 202
+                    return (
+                        {
+                            "error": "Analysis is still in progress",
+                            "status": status,
+                            "parameters": parameters,
+                        },
+                        202,
+                    )
+                elif status["state"] == "terminated":
+                    return (
+                        {
+                            "error": "Analysis was not successful",
+                            "status": status,
+                            "parameters": parameters,
+                        },
+                        404,
+                    )
+                elif status["state"] in ("scheduling", "waiting"):
+                    return (
+                        {
+                            "error": "Analysis is being scheduled",
+                            "status": status,
+                            "parameters": parameters,
+                        },
+                        202,
+                    )
                 else:
                     # Can be:
                     #   - return 500 to user as this is our issue
                     raise ValueError(f"Unreachable - unknown job state: {status}")
             except OpenShiftNotFound:
                 pass
-        return {
-            'error': f'Requested result for analysis {analysis_id!r} was not found',
-            'parameters': parameters
-        }, 404
+        return (
+            {
+                "error": f"Requested result for analysis {analysis_id!r} was not found",
+                "parameters": parameters,
+            },
+            404,
+        )
 
 
 def _get_job_log(parameters: dict, name_prefix: str, namespace: str):
     """Get job log based on analysis id."""
-    job_id = parameters.get('analysis_id')
+    job_id = parameters.get("analysis_id")
     if not job_id.startswith(name_prefix):
-        return {
-            'error': 'Wrong analysis id provided',
-            'parameters': parameters
-        }, 400
+        return {"error": "Wrong analysis id provided", "parameters": parameters}, 400
 
-    return {
-        'parameters': parameters,
-        'log': _OPENSHIFT.get_job_log(job_id, namespace=namespace)
-    }, 200
+    return (
+        {
+            "parameters": parameters,
+            "log": _OPENSHIFT.get_job_log(job_id, namespace=namespace),
+        },
+        200,
+    )
 
 
 def _get_job_status(parameters: dict, name_prefix: str, namespace: str):
     """Get status for a job."""
-    job_id = parameters.get('analysis_id')
+    job_id = parameters.get("analysis_id")
     if not job_id.startswith(name_prefix):
-        return {
-            'error': 'Wrong analysis id provided',
-            'parameters': parameters
-        }, 400
+        return {"error": "Wrong analysis id provided", "parameters": parameters}, 400
 
     status = _OPENSHIFT.get_job_status_report(job_id, namespace=namespace)
-    return {
-        'parameters': parameters,
-        'status': status
-    }
+    return {"parameters": parameters, "status": status}
 
 
 def _do_run(parameters: dict, runner: typing.Callable, **runner_kwargs):
     """Run the given job - a generic method for running any analyzer, solver, ..."""
-    return {
-        'analysis_id': runner(**parameters, **runner_kwargs),
-        'parameters': parameters,
-        'cached': False
-    }, 202
+    return (
+        {
+            "analysis_id": runner(**parameters, **runner_kwargs),
+            "parameters": parameters,
+            "cached": False,
+        },
+        202,
+    )
+
