49048bbacde16c2306a49c12dc0e09ca5a1afaa2
Fridolin Pokorny <fridolin@redhat.com>
2019-09-17 17:38:40 +0200

    Fix wring import when importing performance models

--- thoth/management_api/api_v1.py
+++ thoth/management_api/api_v1.py
@@ -25,7 +25,7 @@ from thoth.common import OpenShift
 from thoth.common.exceptions import NotFoundException as OpenShiftNotFound
 from thoth.storages import __version__ as thoth_storages_version
 from thoth.storages import GraphDatabase
-from thoth.storages.graph.models import ALL_PERFORMANCE_MODELS
+from thoth.storages.graph.models_performance import ALL_PERFORMANCE_MODELS
 from thoth.storages import SolverResultsStore
 from thoth.storages import DependencyMonkeyReportsStore
 from thoth.storages import PackageAnalysisResultsStore
